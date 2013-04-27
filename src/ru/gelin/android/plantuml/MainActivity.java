342d6e9fcb83872cc043302f3ad3fb0407718364
Denis Nelubin <dnelubin@gmail.com>
2013-04-27 12:42:13 +0700

    Updated icons. Added action bar to main activity.

--- src/ru/gelin/android/plantuml/MainActivity.java
+++ src/ru/gelin/android/plantuml/MainActivity.java
@@ -3,11 +3,12 @@ package ru.gelin.android.plantuml;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.SherlockPreferenceActivity;
 
 /**
  *  Main activity which displays how-to-use instructions and allows to set the PlantUML server URL.
  */
-public class MainActivity extends PreferenceActivity {
+public class MainActivity extends SherlockPreferenceActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
